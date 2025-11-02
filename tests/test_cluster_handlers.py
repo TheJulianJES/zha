7067d3f56117b279e826e790407082b0d9a31b72
TheJulianJES <TheJulianJES@users.noreply.github.com>
2025-11-02 06:12:31 +0100

    Update cluster handler tests

--- tests/test_cluster_handlers.py
+++ tests/test_cluster_handlers.py
@@ -545,7 +545,7 @@ def test_cluster_handler_registry() -> None:
         for model_quirk_list in manufacturer.values():
             for quirk in model_quirk_list:
                 qid: set[str] | str = getattr(quirk, ATTR_QUIRK_ID, set())
-                quirk_id: set[str] = {qid} if isinstance(qid, str) else qid
+                quirk_ids: set[str] = {qid} if isinstance(qid, str) else qid
 
                 device_description: dict[str, dict[str, Any]] = getattr(
                     quirk, "replacement", None
@@ -562,7 +562,7 @@ def test_cluster_handler_registry() -> None:
                             cluster_id = cluster_id.cluster_id
                         if cluster_id not in cluster_quirk_id_map:
                             cluster_quirk_id_map[cluster_id] = {None}
-                        cluster_quirk_id_map[cluster_id].update(quirk_id)
+                        cluster_quirk_id_map[cluster_id].update(quirk_ids)
 
     for (
         cluster_id,
