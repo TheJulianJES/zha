23eef76bf58e7df78b1538ee66979735ac77f4fc
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-03-20 01:49:11 +0100

    Fix NaN values being recognized as non-values (#712)

--- zha/application/platforms/sensor/__init__.py
+++ zha/application/platforms/sensor/__init__.py
@@ -10,6 +10,7 @@ from datetime import date, datetime, timedelta
 import enum
 import functools
 import logging
+import math
 import numbers
 import typing
 from typing import TYPE_CHECKING, Any, cast
@@ -364,6 +365,9 @@ class Sensor(BaseSensor):
         self, value: int | float, *, attr_def: foundation.ZCLAttributeDef | None = None
     ) -> bool:
         """Ignore non-value numerical values."""
+        if isinstance(value, float) and math.isnan(value):
+            return True
+
         if attr_def is None:
             attr_def = self._attr_def
 
