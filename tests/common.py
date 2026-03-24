76217c5cbca9023bd12be019674ab49f74f0610f
TheJulianJES <TheJulianJES@users.noreply.github.com>
2026-03-24 07:19:52 +0100

    Fix test mock

--- tests/common.py
+++ tests/common.py
@@ -60,7 +60,7 @@ def patch_cluster_for_testing(cluster: zigpy.zcl.Cluster) -> None:
         ]
     )
     cluster.configure_reporting_multiple = AsyncMock(
-        return_value=zcl_f.ConfigureReportingResponse.deserialize(b"\x00")[0]
+        side_effect=lambda config: dict.fromkeys(config, zcl_f.Status.SUCCESS)
     )
     cluster.read_attributes = AsyncMock(wraps=cluster.read_attributes)
     cluster.read_attributes_raw = AsyncMock(side_effect=_read_attribute_raw)
